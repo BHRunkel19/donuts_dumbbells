aa485bbc0222d576a60653046309d7b4ac0e97b7
BubbaBean <brandon.runkel@gmail.com>
2017-07-20 16:47:49 -0400

    buttons now work, functions redefined and final styling in progress

--- donuts_dumbbells.js
+++ donuts_dumbbells.js
@@ -14,37 +14,71 @@ main_content.appendChild(donuts);
 main_content.appendChild(dumbbells);
 body.appendChild(main_content);
 
-//
+
 // // Map API from Google + Place marker functionality
-//
-var map;
-var infoWindow;
 
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 35.7796, lng: -78.6382},
-        zoom: 12
-      });
-      infoWindow = new google.maps.InfoWindow;
-//
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+//Global Variables
+  var map;
+  var infowindow
+//-----------------------
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-       }
+function initMap() {
+var raleigh = {lat: 35.7770, lng: -78.6382};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: raleigh,
+    zoom: 16,
+  });
+}
+
+//Donuts button will print relevant results
+donuts.addEventListener('click', function(){
+var service = new google.maps.places.PlacesService(map);
+var raleigh = {lat: 35.7780, lng: -78.6382};
+  service.nearbySearch({
+    location: raleigh,
+    radius: 500,
+    keyword: ["donuts"],
+  }, callback);
+})
+
+//Dumbbells button will print relevant results
+
+dumbbells.addEventListener('click', function(){
+var service = new google.maps.places.PlacesService(map);
+var raleigh = {lat: 35.7780, lng: -78.6382};
+  service.nearbySearch({
+    location: raleigh,
+    radius: 500,
+    keyword: ['gym'],
+  }, callback);
+})
+
+//---------------------------------------------
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i]);
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+    infoWindow = new google.maps.InfoWindow();
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
 
 // // AIzaSyBDJs9UE0QyxDbT-WEOO05tl4_IAQUo70Q (API KEY)
